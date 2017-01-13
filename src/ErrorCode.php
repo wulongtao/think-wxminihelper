8df51dddff4fa459ffc75e8d34602e8bb4891abc
wulongtao <769418278@qq.com>
2017-01-13 17:40:28 +0800

    微信小程序登录模块

--- src/ErrorCode.php
+++ src/ErrorCode.php
@@ -20,4 +20,7 @@ class ErrorCode
     public static $IllegalIv = -41002;
     public static $IllegalBuffer = -41003;
     public static $DecodeBase64Error = -41004;
+    public static $RequestTokenFailed = -41005;
+    public static $SignNotMatch = -41006;
+    public static $EncryptDataNotMatch = -41007;
 }
