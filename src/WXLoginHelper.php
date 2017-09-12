774871e81cd190e9454046f471db154367e5dc84
wintrue <328945440@qq.com>
2017-09-12 18:19:40 +0800

    Update WXLoginHelper.php
    add a function "get_3rd_session"

--- src/WXLoginHelper.php
+++ src/WXLoginHelper.php
@@ -178,21 +178,12 @@ class WXLoginHelper {
      * @return mixed|string
      */
     public function randomFromDev($len) {
-        $fp = @fopen('/dev/urandom','rb');
-        $result = '';
-        if ($fp !== FALSE) {
-            $result .= @fread($fp, $len);
-            @fclose($fp);
-        }
-        else
-        {
-            trigger_error('Can not open /dev/urandom.');
-        }
-        // convert from binary to string
+        return $this->get_3rd_session($len);
+    }
+    function get_3rd_session($len){
+        $result=mcrypt_create_iv(128, MCRYPT_DEV_URANDOM);
         $result = base64_encode($result);
-        // remove none url chars
         $result = strtr($result, '+/', '-_');
-
         return substr($result, 0, $len);
     }
 }
